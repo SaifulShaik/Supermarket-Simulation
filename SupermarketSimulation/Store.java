9d690ee937744a83e60528f0b438bb0d8240b796
Joe <52431024+joezhuo2@users.noreply.github.com>
2025-11-04 09:23:29 -0500

    added a store class
    saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful saiful

---
+++ SupermarketSimulation/Store.java
@@ -0,0 +1,17 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.List;
+
+/**
+ * Write a description of class Store here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Store extends Actor
+{
+    public final int width = 450;
+    public final int height = 500;
+    
+    public final int maxCustomers = 8;
+    private int currentCustomers;
+}
