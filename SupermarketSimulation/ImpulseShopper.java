c6d8983ffa3da71c0f69b40baa6dd865b8765c08
Joe <52431024+joezhuo2@users.noreply.github.com>
2025-11-19 08:44:51 -0500

    fixed impulse shoppers not spawning

--- SupermarketSimulation/ImpulseShopper.java
+++ SupermarketSimulation/ImpulseShopper.java
@@ -10,34 +10,17 @@ import java.util.ArrayList;
  */
 public class ImpulseShopper extends Customer
 {
-    
     public ImpulseShopper(Node n) 
     {
          //moving speed, budget, node
         //super(2,100,n);
-        super(2.0, 100.0, null, 3, 2, 100);
+        super(2.0, 100.0, n, 10, 5, 600);
         
         rightImages = new GreenfootImage[] {new GreenfootImage("impulseShopper/right1.png"),new GreenfootImage("impulseShopper/right2.png")};
         leftImages = new GreenfootImage[] {new GreenfootImage("impulseShopper/left1.png"),new GreenfootImage("impulseShopper/left2.png")};
         upImages = new GreenfootImage[] {new GreenfootImage("impulseShopper/up1.png"),new GreenfootImage("impulseShopper/up2.png")};
         downImages = new GreenfootImage[] {new GreenfootImage("impulseShopper/down1.png"),new GreenfootImage("impulseShopper/down2.png")};
         
-    }
-    private boolean shoppingDone()
-    {
-        if(shoppingList.size()==0 && cart.size()>0)
-        {
-            return true;
-        }
-        
-        return false;
-    }
-    private void checkingOutAtCashier()
-    {
-        if(shoppingDone() && isTouching(Cashier.class))
-        {
-            
-        }
     }
 }
 
